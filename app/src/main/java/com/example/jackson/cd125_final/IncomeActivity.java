e883bcfb5075cc6cfaf470e8fdb17f7603b0fa6b
jackson ohanian <ohanian3@illinois.edu>
2018-12-06 18:30:07 -0600

    radio buttons are on but something isnt working making new chart, also need an area for adding transactions whole new layoit would be nice maybe linked to a button on home screen and ill change the name of the transaction button because its not what it does anymore.

--- app/src/main/java/com/example/jackson/cd125_final/IncomeActivity.java
+++ app/src/main/java/com/example/jackson/cd125_final/IncomeActivity.java
@@ -4,6 +4,9 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.RadioButton;
+import java.util.ArrayList;
+import android.widget.LinearLayout;
 
 import com.github.mikephil.charting.animation.Easing;
 import com.github.mikephil.charting.charts.PieChart;
@@ -19,6 +22,22 @@ import java.util.ArrayList;
  */
 public class IncomeActivity extends MainActivity{
 
+    /**
+     * List of all radioButtons
+     */
+    ArrayList<RadioButton> radioBtnList = new ArrayList<>();
+
+    /**
+     * List of all selected RadioButtons
+     */
+    ArrayList<Budgets> selBudgets = new ArrayList<>();
+
+    /**
+     * True if any buttons are selected.
+     */
+    boolean selected = false;
+
+
     IncomeActivity () {
         Log.i("this", "made IncomeActivity");
     }
@@ -29,7 +48,21 @@ public class IncomeActivity extends MainActivity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_income);
 
-        makePieChart();
+        // This makes the # of radio buttons
+        for (int i = 0; i < Budgets.getAllBudgets().size(); i++) {
+            radioBtnList.add(new RadioButton(this));
+            LinearLayout layout = findViewById(R.id.rb);
+            layout.addView(radioBtnList.get(i));
+            radioBtnList.get(i).setText(Budgets.getAllBudgets().get(i).getBudgetName());
+        }
+
+
+        // Decides which pe chart to draw TODO
+        if (!selected) {
+            makePieChart();
+        } else {
+            makeSelectedPieChart(selBudgets);
+        }
     }
 
     public void main(String[] args) {
@@ -44,7 +77,6 @@ public class IncomeActivity extends MainActivity{
         finish();
     }
 
-    // open source third-party library for making interactive pie charts
     public void makePieChart() {
         PieChart pieChart = findViewById(R.id.piechart_income);
 
@@ -62,6 +94,8 @@ public class IncomeActivity extends MainActivity{
         double budgetTotal = (double) Budgets.getTotalBudget();
         double percentOfBudget = 0.0;
 
+
+        // Defualt shows all budgets and how much they compose of the total
         for (Budgets i : chartValues) {
             percentOfBudget = ((double) i.getBudgetAmount() / budgetTotal) * 100.0;
             yValues.add(new PieEntry((int) percentOfBudget,i.getBudgetName()));
@@ -79,5 +113,53 @@ public class IncomeActivity extends MainActivity{
         pieChart.setData(pieData);
     }
 
+    public void makeSelectedPieChart(ArrayList<Budgets> selectedBudgets) {
+        PieChart pieChart = findViewById(R.id.piechart_income);
+
+        pieChart.setUsePercentValues(true);
+        pieChart.getDescription().setEnabled(true);
+        pieChart.setExtraOffsets(5,10,5,5);
+        pieChart.setDragDecelerationFrictionCoef(0.9f);
+        pieChart.setTransparentCircleRadius(61f);
+        pieChart.setHoleColor(Color.WHITE);
+        pieChart.animateY(1000, Easing.EasingOption.EaseInOutCubic);
+        ArrayList<PieEntry> yValues = new ArrayList<>();
+
+        double budgetTotal = (double) Budgets.getTotalBudget();
+        double percentOfBudget = 0.0;
+
+
+        // Defualt shows all budgets and how much they compose of the total
+        for (Budgets i : selectedBudgets) {
+            percentOfBudget = ((double) i.getBudgetAmount() / budgetTotal) * 100.0;
+            yValues.add(new PieEntry((int) percentOfBudget,i.getBudgetName()));
+        }
+
+
+
+        PieDataSet dataSet = new PieDataSet(yValues, "Your Budget");
+        dataSet.setSliceSpace(3f);
+        dataSet.setSelectionShift(5f);
+        dataSet.setColors(ColorTemplate.COLORFUL_COLORS);
+        PieData pieData = new PieData((dataSet));
+        pieData.setValueTextSize(10f);
+        pieData.setValueTextColor(Color.YELLOW);
+        pieChart.setData(pieData);
+    }
+
+    public void runRadios(View view) {
+        // Makes a list of selected buttons and calls pie chart creation
+        int index = 0;
+
+        for (RadioButton i : radioBtnList) {
+            if (i.isChecked()) {
+                selected = true;
+                index = radioBtnList.indexOf(i);
+                selBudgets.add(Budgets.getAllBudgets().get(index));
+
+            }
+        }
+
+    }
 
 }
